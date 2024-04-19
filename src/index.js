cba4f2a37af38e41fe036e565bf9d822eba6fe08
Diego HO <diego@Diegos-MacBook-Pro.local>
2024-04-19 12:05:56 -0600

    Fixed export path errors and added build command to package json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-export { default as CallToActionButton } from "./src/components/CallToActionButton";
-export { default as CalendauModal } from "./src/components/CalendauModal";
-export { default as DirectEmbed } from "./src/components/DirectEmbed";
-export { default as DirectPreviewEmbed } from "./src/components/DirectPreviewEmbed";
-export { default as FloatingButton } from "./src/FloatingButton";
-export { default as InlineText } from "./src/InlineText";
+export { default as CallToActionButton } from "./components/CallToActionButton";
+export { default as CalendauModal } from "./components/CalenduModal";
+export { default as DirectEmbed } from "./components/DirectEmbed";
+export { default as DirectPreviewEmbed } from "./components/DirectPreviewEmbed";
+export { default as FloatingButton } from "./components/FloatingButton";
+export { default as InlineText } from "./components/InlineText";
